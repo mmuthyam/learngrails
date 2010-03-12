72d41fb50b2af0b4aceb02d2994a7205dc7ce9c3
mmuthyam <mmutham@zoho.com>
2010-03-12 15:26:56 -0500

    Initial configuration files

---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,33 @@
+dataSource {
+	pooled = true
+	driverClassName = "org.hsqldb.jdbcDriver"
+  //driverClassName = "com.p6spy.engine.spy.P6SpyDriver" // use this driver to enable p6spy logging
+	username = "sa"
+	password = ""
+}
+hibernate {
+    cache.use_second_level_cache=true
+    cache.use_query_cache=true
+    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
+}
+// environment specific settings
+environments {
+	development {
+		dataSource {
+			dbCreate = "update" // one of 'create', 'create-drop','update'
+			url = "jdbc:hsqldb:file:devDB;shutdown=true"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:hsqldb:mem:testDb"
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+		}
+	}
+}
---
+++ grails-app/conf/SecurityConfig.groovy
@@ -0,0 +1,10 @@
+security {
+
+	// see DefaultSecurityConfig.groovy for all settable/overridable properties
+
+	active = true
+
+	loginUserDomainClass = "com.mmuthyam.security.Users"
+	authorityDomainClass = "com.mmuthyam.security.Authority"
+	requestMapClass = "com.mmuthyam.security.Requestmap"
+}
